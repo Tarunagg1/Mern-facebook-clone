a4bb792ccab944ecb5276739c1672b873e1c547f
--add <tarun@iotric.com>
2022-06-21 16:25:34 +0530

    ui done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import { Fragment } from 'react';
 import './App.css';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
+import Register from './pages/register/Register';
 
 function App() {
   return (
     <Fragment>
-      <Home />
+      {/* <Home /> */}
+      {/* <Profile /> */}
+      {/* <Login /> */}
+      <Register />
     </Fragment>
   );
 }
